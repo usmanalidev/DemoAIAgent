84e56622b7b5a655b6b7d606054e3f7a00e3f310
Usman Ali <usman.a@borngroup.com>
2025-07-11 18:15:25 +0300

    cors fix

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,13 @@ const PORT = 5000;
 const cors = require('cors');
 
 app.use(cors({
-  origin: 'http://localhost:3000/'
+  origin: 'http://localhost:3000'
 }));
 
 app.get('/', (req, res) => {
   res.send('Hello from backend!');
 });
+
 
 app.get('/hello', (req, res) => {
   res.json({ message: 'Hello World!' });
